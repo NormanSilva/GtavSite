cf31bbf6f9f972e2b4b1497cc3ecaae02e2f510a
NormanSilva <n.silvazeta@gmail.com>
2022-06-21 14:56:50 -0500

    Co-authored-by: Julian Flores <RuloDeMoebius@users.noreply.github.com> Co-authored-by: Julian Flores <julianfloresdev@gmail.com>

---
+++ js/pago.js
@@ -0,0 +1,121 @@
+/*
+See on github: https://github.com/muhammederdem/credit-card-form
+*/
+
+new Vue({
+  el: "#app",
+  data() {
+    return {
+      currentCardBackground: Math.floor(Math.random()* 25 + 1), // just for fun :D
+      cardName: "",
+      cardNumber: "",
+      cardMonth: "",
+      cardYear: "",
+      cardCvv: "",
+      minCardYear: new Date().getFullYear(),
+      amexCardMask: "#### ###### #####",
+      otherCardMask: "#### #### #### ####",
+      cardNumberTemp: "",
+      isCardFlipped: false,
+      focusElementStyle: null,
+      isInputFocused: false
+    };
+  },
+  mounted() {
+    this.cardNumberTemp = this.otherCardMask;
+    document.getElementById("cardNumber").focus();
+  },
+  computed: {
+    getCardType () {
+      let number = this.cardNumber;
+      let re = new RegExp("^4");
+      if (number.match(re) != null) return "visa";
+
+      re = new RegExp("^(34|37)");
+      if (number.match(re) != null) return "amex";
+
+      re = new RegExp("^5[1-5]");
+      if (number.match(re) != null) return "mastercard";
+
+      re = new RegExp("^6011");
+      if (number.match(re) != null) return "discover";
+      
+      re = new RegExp('^9792')
+      if (number.match(re) != null) return 'troy'
+
+      return "visa"; // default type
+    },
+		generateCardNumberMask () {
+			return this.getCardType === "amex" ? this.amexCardMask : this.otherCardMask;
+    },
+    minCardMonth () {
+      if (this.cardYear === this.minCardYear) return new Date().getMonth() + 1;
+      return 1;
+    }
+  },
+  watch: {
+    cardYear () {
+      if (this.cardMonth < this.minCardMonth) {
+        this.cardMonth = "";
+      }
+    }
+  },
+  methods: {
+    flipCard (status) {
+      this.isCardFlipped = status;
+    },
+    focusInput (e) {
+      this.isInputFocused = true;
+      let targetRef = e.target.dataset.ref;
+      let target = this.$refs[targetRef];
+      this.focusElementStyle = {
+        width: `${target.offsetWidth}px`,
+        height: `${target.offsetHeight}px`,
+        transform: `translateX(${target.offsetLeft}px) translateY(${target.offsetTop}px)`
+      }
+    },
+    blurInput() {
+      let vm = this;
+      setTimeout(() => {
+        if (!vm.isInputFocused) {
+          vm.focusElementStyle = null;
+        }
+      }, 300);
+      vm.isInputFocused = false;
+    }
+  }
+});
+
+
+/*SWEET ALERT*/
+
+const botonFinalizarPago = document.querySelector("[finalizarPago]");
+
+ botonFinalizarPago.addEventListener("click",()=>{
+  swal({
+    title: `¿Seguro que desea confirmar su compra?`,
+    text: `Por favor confirmar`,
+    icon: "warning",
+    buttons: ["Cancelar Compra", "Continuar con el Pago!"],
+    
+   })
+   .then((willDelete) =>{
+    if(willDelete){
+      swal({
+        title: "Excelente tu compra se realizó con éxito",
+        text: `Muchas Gracias por elegirnos!`,
+        icon: "success",
+        timer: 3000,
+        button: false
+      })
+    } else{
+      swal("Te comprendemos, no pasa nada. Muchas gracias!",{
+        icon: "error",
+        timer: 3000,
+        button: false
+      });
+      
+    }
+   })
+ });
+ 
--- js/tienda.js
+++ js/tienda.js
@@ -1,74 +1,51 @@
-/* TAREA PASADA */
-// function ingresoDatos(mensaje) {
-//     let dato = prompt(mensaje);
-//     console.log(dato);
-//     return dato;
-// }
+class Producto{
+    constructor(id, nombre, precio, cantidad) {
+        this.id = id
+        this.nombre = nombre
+        this.precio = precio
+        this.cantidad = cantidad
+    }   
+};
 
-// let usuario = ingresoDatos ("Ingresa tu nombre de usuario")
-// let contrasena = ingresoDatos ("Ingresa tu contraseña");
+const almacen = []
 
-// alert("Bienvenido " + usuario + ", estás en la tienda de GTA V");
-
-// let producto
-// let cantidad
-// function eleccionProducto(){
-//     producto = parseInt( prompt("Elige el producto que desea comprar en nuestra tienda: \n 1. Gorra Rockstar s/.50 \n 2. Camiseta roja Rockstar s/.70 \n 3. Mechero RDR2 s/.120"))
-//     cantidad = parseInt( prompt("Que cantidad desea llevar?"))
-
-// }
- 
-// eleccionProducto();
-
-// switch (producto) {
-//     case 1:
-//         producto = 50;
-//         break
-//     case 2:
-//         producto = 70;
-//         break
-//     case 3:
-//         producto = 120;
-//         break
-// }
-// let resultado = cantidad * producto
-// alert ("Tu monto a pagar es s/. " + resultado)
-
-function Producto(id, nombre, precio){
-    this.id = id
-    this.nombre = nombre
-    this.precio = precio
-}
-
-const productos = []
-productos.push ({ id:1, nombre:"T-shirt Rockstar Negro", precio:50 })
-productos.push({ id:2, nombre:"T-shirt Rockstar Gris", precio:50 })
-productos.push ({ id:3, nombre:"T-shirt Rockstar Rojo", precio:50 })
-productos.push({ id:4, nombre:"T-shirt KIFFLOM", precio:50 })
-productos.push ({ id:5, nombre:"Sudadera Rockstar Games", precio:120 })
-productos.push({ id:6, nombre:"Gorra Rockstar Games", precio:80 })
-productos.push({ id:7, nombre:"Vaso de chupitos RDR2", precio:120 })
-productos.push({ id:8, nombre:"Mechero Zippo RDR2", precio:80 })
-productos.push({ id:9, nombre:"Tomatodo Sprunk", precio:80 })
+almacen.push (new Producto(1, "T-shirt Rockstar Negro", 50, 0));
+almacen.push(new Producto (2, "T-shirt Rockstar Gris", 50, 0));
+almacen.push ( new Producto(3, "T-shirt Rockstar Rojo", 50, 0));
+almacen.push(new Producto(4, "T-shirt KIFFLOM", 50, 0));
+almacen.push (new Producto(5, "Sudadera Rockstar Games", 120, 0));
+almacen.push(new Producto(6, "Gorra Rockstar Games", 80, 0));
+almacen.push(new Producto(7, "Vaso de chupitos RDR2", 120, 0));
+almacen.push(new Producto(8, "Mechero Zippo RDR2", 80, 0));
+almacen.push(new Producto(9, "Tomatodo Sprunk", 80, 0));
 
 
+// ===== DOM & EVENTOS ===
+const lista = document.querySelector(".carrito");
+const carritoIcon = document.querySelector(".carritoIcon");
 
-// alert("Bienvenido a la tienda de GTA V")
 
-
-const lista = document.querySelector(".ventana");
-const carrito = document.querySelector(".submenu");
-
-// carrito.addEventListener("click", () => {
-//     lista.style.display = "flex";
-//     // lista.innerHTML = `<p class= "tituloPeedidos" > Tus Pedidos </p>`
-//     console.log("funciona")
-// })
-
+// Evento dando click al carrito
 let carritoclickeado = false;
-carrito.addEventListener("click", () => {
+carritoIcon.addEventListener("click", () => {
     switch (carritoclickeado) {
         case false:
+            // carrito = JSON.parse(sessionStorage.getItem("carrito"));
+
+            // carrito.forEach(element => {
+            //     lista.innerHTML += `
+            //     <div class="productoElegido">
+            //          <div>
+            //             <p class="nameCarrito"> ${element.nombre} </p>
+            //          </div>
+            //          <div>
+            //             <p class="precioCarrito"> s/.${element.precio} </p>
+            //          </div>
+            //          <div>
+            //             <p class="cantidad"> cantidad: ${element.cantidad} </p>
+            //     </div>`;
+            // })
+            
             lista.style.display = "flex";
             carritoclickeado = true;
             break;
@@ -79,28 +56,88 @@ carrito.addEventListener("click", () => {
     }
 });
 
+//Función agregar productos al carrito
+let carrito = [];
 
+function añadirAlCarrito(parametro) {
+          
+            if (carrito.some(element => element.id == parametro)) {
+                carrito.find(element => element.id == parametro).cantidad++;                
+                
+            
+            } else {
+                let prdt = almacen.find(element => element.id == parametro);              
+                prdt.cantidad++;
+                carrito.push(prdt);
+            }        
+        } 
 
-
-
-const agregar = document.getElementsByClassName("btnAgregar"); /*Esto contiene todo mis elementos del html que tienen clase "btnAgregar" */
-agregar.addEventListener("click", ()=>{
-    //funcion que agrege al carrito
-    
-})
-
-
-
-
+//Seleccionando productos al carrito de compra
+const agregar = document.getElementsByClassName("btnAgregar"); 
 console.log(agregar)
 
+let btnPagar = true;
+let precioFinal;
+let carritoRed;
+
+ for(let i=0; i<almacen.length; i++){
+     agregar[i].addEventListener("click", ()=>{
+        añadirAlCarrito(almacen[i].id);
+            lista.innerHTML = `
+                <div>
+                    <p class="tituloPedidos"> Tus Pedidos </p>
+                </div>
+                <div class="contenedor-carrito">
+
+                </div>`;
+                const carritoContenedor = document.querySelector(".contenedor-carrito");
+
+            carrito.forEach(element => {
+                
+                //Y esto agrega cada producto del carrito a continuación y sin pisar el título.
+                carritoContenedor.innerHTML += `
+                <div class="productoElegido">
+                        <div class="nameCarrito">
+                            <p> ${element.nombre} </p>
+                        </div>
+                        <div class="cantidad">
+                            <p> (${element.cantidad}) </p>
+                        </div>
+                        <div class="precioCarrito">
+                            <p> s/.${element.precio * element.cantidad}  </p>
+                        </div>
+                </div>
+                <hr>`;
+            });
+
+            carritoRed = [...carrito];
+
+            (carrito.length <= 1) ? precioFinal = carrito[0].precio * carrito[0].cantidad : precioFinal = carritoRed.reduce((a,b) => (a.precio * a.cantidad) + (b.precio * b.cantidad));
+
+            lista.innerHTML += `
+            <div class="carrito-total-a-pagar">
+                <h4>Total a Pagar: ${precioFinal}</h4>
+            </div>
+            <button class ="btnComprar"><a href="../pages/pago.html"> Comprar </a></button>`; 
 
 
+       sessionStorage.setItem(`carrito`, JSON.stringify(carrito));
+     }) 
+ };
 
+/*class="comprar-carrito"*/ //clase del button
+//  const btnComprar = docuemnt.querySelector(".comprar-carrito");
+//  const divTarjeta = document.querySelector(".tarjeta");
+//  const divAllCards = document.querySelector(".allCards");
 
+//  btnComprar.addEventListener("click", () =>{
+//     console.log("cambiar displays");
+//     divTarjeta.style.display = "flex";
+//     divAllCards.style.display = "none";
+//  });
 
+//  const btnFinalizarCompra = docuemnt.querySelector(".btn-finalizar-compra");
 
+//  btnFinalizarCompra.addEventListener("click", ()=>{
 
-
-
-
+//  });
